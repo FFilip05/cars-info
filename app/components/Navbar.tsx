c24f494c059d29af1ff227249452de6e9740a73e
Filip <filipfuks2005@gmail.com>
2024-02-19 16:28:05 +0100

    added navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Link from "next/link";
+// import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className=" px-32 py-4   bg-sky-400">
+  <div className="flex justify-between items-center">
+    <div className="font-bold">CarX</div>
+    <div className="hidden md:flex space-x-4">
+      <Link href="/cars" className="font-medium">Cars</Link>
+      <Link href="/create" className="font-medium">Create</Link>
+      <Link href="#" className="font-medium">Sign Up</Link>
+      <Link href="#" className="font-medium">Log In</Link>
+    </div>
+  </div>
+</div>
+
+    // <Navbar>
+    //   <NavbarBrand>
+    
+    //     <p className="font-bold text-inherit">CarX</p>
+    //   </NavbarBrand>
+    //   <NavbarContent className="hidden sm:flex gap-4" justify="center">
+    //     <NavbarItem>
+    //       <Link color="foreground" className="font-medium" href="/cars">
+    //         Cars
+    //       </Link>
+    //     </NavbarItem>
+    //     <NavbarItem isActive>
+    //       <Link href="#" className="font-medium" aria-current="page">
+    //         Customers
+    //       </Link>
+    //     </NavbarItem>
+    //     <NavbarItem>
+    //       <Link color="foreground" className="font-medium" href="/cars/create">
+    //         Create
+    //       </Link>
+    //     </NavbarItem>
+    //   </NavbarContent>
+    //   <NavbarContent justify="end">
+    //     <NavbarItem className="hidden lg:flex">
+    //       <Link href="#" className="font-medium">Login</Link>
+    //     </NavbarItem>
+    //     <NavbarItem>
+    //       <Button as={Link} color="primary" href="#" variant="flat" className="font-medium">
+    //         Sign Up
+    //       </Button>
+    //     </NavbarItem>
+    //   </NavbarContent>
+    // </Navbar>
+  );
+}
