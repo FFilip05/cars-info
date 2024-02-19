db1ae7cfc48b706e6ed51e99be1b6d1331a83fb9
Filip <filipfuks2005@gmail.com>
2024-02-19 16:32:51 +0100

    added create page for adding cars to db

---
+++ app/cars/create/page.tsx
@@ -0,0 +1,21 @@
+import { createCar } from "@/app/lib/actions";
+export default async function Page() {
+
+    return(
+      <div className="border border-black capitalize flex justify-center h-lvh">
+        <div className="flex flex-col m-auto w-1/2 border border-black p-4 ">
+      <h1 className="font-bold text-center">Add Car form:</h1>   
+     <form  action={createCar} className=" flex flex-col">
+     <label htmlFor="make" className="font-semibold my-3">
+      make
+    </label>
+    <input type="text" name="make" id="make" required/>
+    <label htmlFor="name" className="font-semibold my-3">
+      name
+    </label>
+    <input type="text" name="name" id="name" required />
+    <button type="submit" className="font-bold bg-rose-600 mx-auto px-5 py-3 rounded-lg" >Submit</button>
+  </form>
+  </div></div>
+    )
+  }
