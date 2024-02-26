0e2de0fadb44bd7b798a3176fb04f620019617e6
Filip <filipfuks2005@gmail.com>
2024-02-26 14:38:14 +0100

    modified page for single car

--- app/cars/[id]/page.tsx
+++ app/cars/[id]/page.tsx
@@ -1,4 +1,5 @@
-import CarCard from '@/app/components/car-card';
+import CarCard from '@/app/components/CarCard';
+import { updateCar } from '@/app/lib/actions';
 import { Car } from '@/types/car';
 
 async function getData(id:string) {
@@ -15,11 +16,20 @@ export default async function Page({ params }: { params: { id: string} }) {
 const data = await getData(params.id)
 const Car: Car = await data;
    return (
-       <div>
-           <h1>Cars By Id:</h1>
-           <div>
-                  <CarCard  params={Car} />
-           </div>
-       </div>
+    <div className="border border-black capitalize flex justify-center h-lvh">
+    <div className="flex flex-col m-auto w-1/2 border border-black p-4 ">
+  <h1 className="font-bold text-center">Update Car form:</h1>   
+ <form  action={updateCar} className=" flex flex-col">
+ <label htmlFor="make" className="font-semibold my-3">
+  make
+</label>
+<input type="text" name="make" id="make" required value={Car.make}/>
+<label htmlFor="name" className="font-semibold my-3">
+  name
+</label>
+<input type="text" name="name" id="name" required value={Car.name} />
+<button type="submit" className="font-bold bg-rose-600 mx-auto px-5 py-3 rounded-lg" >Submit</button>
+</form>
+</div></div>
    );
 }
