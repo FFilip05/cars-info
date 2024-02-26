446abe0b1f33e2d90666cc968eacb1c32f4c557c
Filip <filipfuks2005@gmail.com>
2024-02-26 14:21:11 +0100

    added new types

--- types/car.ts
+++ types/car.ts
@@ -2,4 +2,35 @@ export type Car = {
     id: string;
     make: string;
     name: string;
-   };
+    engines: Engine[]; 
+    offers: Offer[]; 
+  };  
+
+export type Engine = {
+    id: string;
+    name: string;
+    displacement: string;
+    hp: number;
+    tq: number;
+    cars: Car[]; 
+  };
+  
+  export type Offer = {
+    id: string;
+    title: string;
+    desc: string;
+    user: User; 
+    userId: string;
+    car: Car; 
+    carId: string;
+    price: number;
+    mileage: number;
+    year: number;
+    createdAt: Date;
+  };
+  
+  export type User = {
+    id: string;
+    offers: Offer[]; 
+  };
+  
